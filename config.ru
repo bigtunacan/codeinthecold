2527412d75576e646004c86c07274b9e390ceb4c
Joiey Seeley <joiey.seeley@gmail.com>
2014-11-24 09:12:21 -0600

    First commit

---
+++ config.ru
@@ -0,0 +1,5 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require "rubygems"
+require './main'
+
+run Sinatra::Application
---
+++ main.rb
@@ -0,0 +1,54 @@
+require 'base64'
+require 'rubygems'
+require 'sinatra'
+
+Dir["./helpers/*.rb"].each{|file| require file}
+Dir["./routes/*.rb"].each{|file| require file}
+
+get '/' do
+  erb :index
+end
+
+get '/entries' do
+  erb :entries
+end
+
+get '/blog' do
+  erb :blog
+end
+
+get '/jobs' do
+  erb :jobs
+end
+
+get '/organizers' do
+  erb :organizers
+end
+
+get '/contestants' do
+  erb :contestants
+end
+
+get '/auth/signin' do
+  'Coming Soon...'
+end
+
+get '/auth/register' do
+  'Coming Soon'
+end
+
+get '/auth/twitter' do
+  'Coming Soon'
+end
+
+get '/auth/admin' do
+  'Coming Soon'
+end
+
+get '/rules' do
+  erb :rules
+end
+
+get '/conduct' do
+  erb :conduct
+end
